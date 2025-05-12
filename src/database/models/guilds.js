528693b195e3ca1dff878914416e408c23026c9d
lucaas-tavares <eu.ikki.dev@gmail.com>
2025-05-12 16:50:28 -0300

    🗃️ db: correct model name and update schema defaults for guilds and users

--- src/database/models/guilds.js
+++ src/database/models/guilds.js
@@ -2,14 +2,24 @@ const mongoose = require('mongoose');
 
 const guildSchema = new mongoose.Schema({
     _id: { type: String, required: true },
+
     lastShopUpdate: { type: Date, default: Date.now },
     availableColors: { type: Array, default: [] },
     lastShopMessageId: { type: String, default: null },
+    colors: {
+        type: [
+            {
+                id: { type: String, default: null },
+                rarity: { type: String, default: null },
+                price: { type: Number, default: null },
+            }
+        ], default: []
+    },
     lastBumpTime: { type: Date, default: null },
     lastBumpUser: { type: String, default: null }
 
 });
 
 module.exports = {
-    Guildas: mongoose.model('Guildas', guildSchema)
+    Guilds: mongoose.model('Guilds', guildSchema)
 };
--- src/database/models/users.js
+++ src/database/models/users.js
@@ -6,10 +6,18 @@ const usersModel = new Schema({
     xp: { type: Number, default: 0 },
     level: { type: Number, default: 1 },
     lastMessage: { type: String, default: '' },
-    lastMessageTime: { type: Date, default: Date.now },
+    lastMessageTime: { type: Date, default: null },
     bumps: { type: Number, default: 0 },
     streak: { type: Number, default: 0 },
-    achievements: { type: [String], default: null },
+    achievements: {
+        type: [
+            {
+                id: { type: String, required: true },
+                unlockedAt: { type: Date, default: Date.now }
+            }
+        ],
+        default: []
+    },      
     colorsInventory: { type: [String], default: [] },
     equippedColor: { type: String, default: null }
 
