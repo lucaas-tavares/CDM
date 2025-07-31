95f1538b041f707ff2a7d84b0639bd182ef8954d
lucaas-tavares <eu.ikki.dev@gmail.com>
2025-07-31 18:18:14 -0300

    ➕ feat: implements new parameter

--- src/discord/commands/prefix/dev/eval.js
+++ src/discord/commands/prefix/dev/eval.js
@@ -1,12 +1,11 @@
 const { inspect } = require('util');
-const db = require('../../../../database/models/users')
 
 module.exports = {
     name:'eval',
     aliases:['e'],
     description:'「Util」eval',
     devOnly: true,
-    run: async (client, message, args, userdb) => {
+    run: async (client, message, args, userdb, guildDb) => {
       try {
         const code = args.join(' ');
 
--- src/discord/events/message/messageCreate.js
+++ src/discord/events/message/messageCreate.js
@@ -1,64 +1,110 @@
 const colorize = require('strcolorize');
 const User = require('../../../database/models/users');
-const { Guilds } = require('../../../database/models/guilds')
+const { Guilds } = require('../../../database/models/guilds');
 const XP = require('../../../functions/module/xp');
 
 module.exports = {
-    type: 'messageCreate',
-    run: async (client, message) => {
-        if (message.author.bot || !message.guild) return;
-        if (!client.guildsOnly.includes(message.guild.id)) return;
+  type: 'messageCreate',
+  run: async (client, message) => {
+    if (message.author.bot || !message.guild) return;
+    if (!client.guildsOnly.includes(message.guild.id)) return;
 
-        let userdb = await User.findById(message.author.id)
-        if (!userdb) {
-            userdb = await new User({
-                _id: message.author.id,
-                xp: 0,
-                level: 1,
-                points: 0,
-                lastMessage: message.content,
-                lastMessageTime: Date.now()
-            }).save().catch(err => console.log(colorize(`#redBg [User] - Não foi possível criar o usuário no banco de dados`), err));
-        }
-
-        let guildDb = await Guilds.findById(message.guild.id)
-        if (!guildDb) {
-            guildDb = await new Guilds({
-                _id: message.guild.id,
-            }).save().catch(err => console.log(colorize(`#redBg [User] - Não foi possível criar a guilda no banco de dados`), err));
-        }
-
-        await XP(client, message, userdb);
-        if (!message.content.startsWith(client.prefix)) return
-
-        const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
-        const cmdName = args.shift().toLowerCase();
-        const command = client.prefixCommands.get(cmdName) || client.prefixCommands.find(als => als.aliases?.includes(cmdName));
-
-        if (!command) {
-            return message.reply(client.formatEmoji(`#e:errado Não foi possível localizar esse comando. Utilize \`${client.prefix}help\`.`));
-        }
-
-        const cooldownKey = `${message.author.id}-${command.name}`;
-
-        if (!client.developers.includes(message.author.id)) {
-            if (client.cooldowns.has(cooldownKey)) {
-                const remaining = Math.trunc(client.cooldowns.get(cooldownKey) / 1000);
-                return message.channel.send(
-                    client.formatEmoji(`#e:relogio Você poderá utilizar comandos novamente em: ${remaining}s`)
-                );
-            }
-
-            client.cooldowns.set(cooldownKey, Date.now() + 7000);
-            setTimeout(() => {
-                client.cooldowns.delete(cooldownKey);
-            }, 7000);
-        }
-
-        try {
-            command.run(client, message, args, userdb);
-        } catch (error) {
-            console.log(colorize(`#redBg [Command] - Houve um erro ao tentar executar o comando`), error);
-        }
+    if (message.content.replace('!', '').startsWith(`<@${client.user.id}>`)) {
+      return message.reply(
+        client.formatEmoji(
+          `#planta ・Oi, ${message.author}, sabia que meus comandos são em slash? Não? Não sabia? Pois agora está sabendo.\n> → utilize \`/\` para ver os meus comandos.\n> Algumas sugestões → \`/perfil\` - \`/loja\` - \`/level\``
+        )
+      );
     }
+
+    let userdb = await User.findById(message.author.id);
+    if (!userdb) {
+      userdb = await new User({
+        _id: message.author.id,
+        xp: 0,
+        level: 1,
+        points: 0,
+        lastMessage: message.content,
+        lastMessageTime: Date.now(),
+      })
+        .save()
+        .catch((err) =>
+          console.log(
+            colorize(
+              `#redBg [User] - Não foi possível criar o usuário no banco de dados`
+            ),
+            err
+          )
+        );
+    }
+
+    let guildDb = await Guilds.findById(message.guild.id);
+    if (!guildDb) {
+      guildDb = await new Guilds({
+        _id: message.guild.id,
+      })
+        .save()
+        .catch((err) =>
+          console.log(
+            colorize(
+              `#redBg [User] - Não foi possível criar a guilda no banco de dados`
+            ),
+            err
+          )
+        );
+    }
+
+    await XP(client, message, userdb);
+    if (!message.content.startsWith(client.prefix)) return;
+
+    const args = message.content
+      .slice(client.prefix.length)
+      .trim()
+      .split(/ +/g);
+    const cmdName = args.shift().toLowerCase();
+    const command =
+      client.prefixCommands.get(cmdName) ||
+      client.prefixCommands.find((als) => als.aliases?.includes(cmdName));
+
+    if (!command) {
+      return message.reply(
+        client.formatEmoji(
+          `#e:errado Não foi possível localizar esse comando. Utilize \`${client.prefix}help\`.`
+        )
+      );
+    }
+
+    if (command.devOnly && !client.developers.includes(message.author.id)) {
+      return message.reply(client.formatEmoji(`#e:errado se saia`));
+    }
+
+    const cooldownKey = `${message.author.id}-${command.name}`;
+
+    if (!client.developers.includes(message.author.id)) {
+      if (client.cooldowns.has(cooldownKey)) {
+        const remaining = Math.trunc(client.cooldowns.get(cooldownKey) / 1000);
+        return message.channel.send(
+          client.formatEmoji(
+            `#e:relogio Você poderá utilizar comandos novamente em: <t:${remaining}:R>s`
+          )
+        );
+      }
+
+      client.cooldowns.set(cooldownKey, Date.now() + 7000);
+      setTimeout(() => {
+        client.cooldowns.delete(cooldownKey);
+      }, 7000);
+    }
+
+    try {
+      command.run(client, message, args, userdb, guildDb);
+    } catch (error) {
+      console.log(
+        colorize(
+          `#redBg [Command] - Houve um erro ao tentar executar o comando`
+        ),
+        error
+      );
+    }
+  },
 };
