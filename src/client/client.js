e89a9095876d994522b8fa0f499cfb4348ab6761
lucaas-tavares <eu.ikki.dev@gmail.com>
2025-07-31 18:12:49 -0300

    ✨ feat: replace 'strcolorize' with '@kauzx/logger' for improved logging

--- src/client/client.js
+++ src/client/client.js
@@ -1,45 +1,55 @@
-const { Client, Collection } = require('discord.js')
-const colorize = require('strcolorize');
-const { loadPrefixCommands, loadSlashCommands, setupEvents, loadComponents } = require('./handlers')
-const { formatEmoji } = require('../functions/utils/FormatEmoji/index')
-const { config } = require('./config');
+const { Client, Collection } = require("discord.js");
+const { logger, icon } = require("@kauzx/logger");
+const {
+  loadPrefixCommands,
+  loadSlashCommands,
+  setupEvents,
+  loadComponents,
+} = require("./handlers");
+const { formatEmoji } = require("../functions/utils/FormatEmoji/index");
+const { config } = require("./config");
+require('../functions/module/antiCrash')
 
 module.exports = class lucy extends Client {
-    constructor(options) {
-        super({ intents: options.intents })
+  constructor(options) {
+    super({ intents: options.intents });
 
-        this.developers = options.developers;
-        this.guildsOnly = options.guildsOnly;
-        this.prefix = options.prefix;
-        this.token = options.token;
-        this.config = config;
+    this.developers = options.developers;
+    this.guildsOnly = options.guildsOnly;
+    this.prefix = options.prefix;
+    this.token = options.token;
+    this.config = config;
 
-        this.commands = new Collection()
-        this.prefixCommands = new Collection();
-        this.components = new Collection();
-        this.cooldowns = new Collection();
+    this.commands = new Collection();
+    this.prefixCommands = new Collection();
+    this.components = new Collection();
+    this.cooldowns = new Collection();
 
-        this.formatEmoji = formatEmoji;
-        this.logSummary = [];
+    this.formatEmoji = formatEmoji;
+    this.logSummary = [];
+  }
+
+  async setup() {
+    try {
+      this.commands = loadSlashCommands(this, this.logSummary);
+      this.prefixCommands = loadPrefixCommands(this, this.logSummary);
+      setupEvents(this, this.logSummary);
+      loadComponents(this, this.logSummary);
+
+      await super.login(this.token);
+      this.displaySummary();
+      logger.style(
+        `{bold.cyan success} - connected in {bold.magenta ${this.user.tag}}`
+      );
+    } catch (error) {
+     logger.error("Erro ao conectar o cliente:", error);
     }
+  }
 
-    async setup() {
-        try {
-            this.commands = loadSlashCommands(this, this.logSummary);
-            this.prefixCommands = loadPrefixCommands(this, this.logSummary);
-            setupEvents(this, this.logSummary);
-            loadComponents(this, this.logSummary);
-    
-            await super.login(this.token);
-            this.displaySummary();
-            console.log(colorize(`#greenBright[success] Conectado em #bold magenta[${this.user.tag}]`));
-        } catch(error) {
-            console.error("Erro ao conectar o cliente:", error);
-        }
-    }
-
-    displaySummary() {
-        console.log(colorize("#bold black[---- ⚙ Aplicação ----]", false));//⚙
-        this.logSummary.forEach((log) => console.log(colorize(`#greenBright[success] ${log}`)));
-    }
-}
+  displaySummary() {
+    logger.style(`{bold.black ---- ${icon.debug} Application ----}`, false); //⚙
+    this.logSummary.forEach((log) =>
+      logger.style(`{cyan success} ${log}`)
+    );
+  }
+};
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { config } = require('./client/config');
 
 const client = new lucy ({
     intents: 34759,
-    prefix: "l.",
-    developers: ["1005290241743143043", "967486659560079420"],
-    guildsOnly: ["1327425233388568576","1093595922979631224", "1295786799477690481"],
+    prefix: 'c.',
+    developers: ['1005290241743143043', '1065051526768373810'],
+    guildsOnly: ['1327425233388568576','1093595922979631224', '1295786799477690481'],
     token: config.token,
 });
 
